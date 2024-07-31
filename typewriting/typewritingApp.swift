a72c4fd44dde74360f8d0810615b1d8a1a474772
Leonie <leoniele1990@gmail.com>
2024-07-31 09:23:25 +0100

    Initial Commit

---
+++ typewriting/typewritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  typewritingApp.swift
+//  typewriting
+//
+//  Created by Scholar on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct typewritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
